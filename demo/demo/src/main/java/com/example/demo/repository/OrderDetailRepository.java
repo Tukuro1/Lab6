048e35787b1e4a2f93936e35f2b58c2e5794f442
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-06-18 22:32:15 +0700

    ok1

--- demo/demo/src/main/java/com/example/demo/repository/OrderDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.Model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
