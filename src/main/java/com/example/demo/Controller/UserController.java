6ad70cd03a79f333f55c8efb730545a678565b48
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-06-18 14:28:25 +0700

    ok

---
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+@Controller // Đánh dấu lớp này là một Controller trong Spring MVC.
+@RequestMapping("/")
+@RequiredArgsConstructor
+
+public class UserController {
+    private final UserService userService;
+    @GetMapping("/login")
+    public String login() {
+        return "user/login";
+    }
+    @GetMapping("/register")
+    public String register(@NotNull Model model) {
+        model.addAttribute("user", new User());
+        return "user/register";
+    }
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("user") User user,
+                                   @NotNull BindingResult bindingResult,
+                                   Model model) {
+        if (bindingResult.hasErrors()) { // Kiểm tra nếu có lỗi validate
+            var errors = bindingResult.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toArray(String[]::new);
+            model.addAttribute("errors", errors);
+            return "users/register"; // Trả về lại view "register" nếu có lỗi
+        }
+        userService.save(user);
+        return "redirect:/login";
+    }
+}
--- src/main/java/com/hutech/webbanhang/repository/OrderDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hutech.webbanhang.repository;
-
-import com.hutech.webbanhang.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
