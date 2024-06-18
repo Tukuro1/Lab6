1e5ee0d393e4570ef763f25e2ae98bcda0bd229d
Nguyễn Tú <tunguyennhuxyz@gmail.com>
2024-06-18 14:31:25 +0700

    xoa

--- demo/demo/src/main/java/com/example/demo/repository/ShipperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Model.Shipper;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShipperRepository  extends JpaRepository<Shipper, Long> {
-}
